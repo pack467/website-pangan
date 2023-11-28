f8983e348cbaa24766726771afcff3b7d17cfab5
anandafiqri <anandafiqri1403@gmail.com>
2023-11-28 13:14:01 +0700

    add user authorize

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import authRoute from "./auth";
 import productRoute from "./product";
 import productTypeRoute from "./productType";
+import cartRoute from "./cart";
 
 export default Router()
   .use("/auth", authRoute)
   .use("/product-type", productTypeRoute)
-  .use("/product", productRoute);
+  .use("/product", productRoute)
+  .use("/cart", cartRoute);
